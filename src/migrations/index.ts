e241bee0ab7040cdf488be2d81d68935c5f9250a
Ubuntu <ubuntu@ip-172-31-24-88.ap-southeast-2.compute.internal>
2026-06-19 00:04:53 +0000

    chore: add initial SQLite migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260619_000228 from './20260619_000228';
+
+export const migrations = [
+  {
+    up: migration_20260619_000228.up,
+    down: migration_20260619_000228.down,
+    name: '20260619_000228'
+  },
+];
